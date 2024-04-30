2416c57f24045f19d3904877b487d5feabf0e97b
Rafael Campos <rafaelcampos04@hotmail.com>
2024-04-30 18:06:45 -0400

    almost done

--- practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -4,27 +4,35 @@ class View {
   constructor(game, containerEle) {
     this.game = game;
     this.containerEle = containerEle;
-    this.setupBoard()
+    this.setupBoard();
   }
   
   setupBoard() {
     const grid = document.createElement('ul');
-    this.containerEle.appendChild(grid)
+    this.containerEle.appendChild(grid);
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         const gridCell = document.createElement('li');
         gridCell.dataset.row = i;
         gridCell.dataset.col = j;
-        grid.appendChild(gridCell)
+        grid.appendChild(gridCell);
+        gridCell.addEventListener("click", this.handleClick.bind(this));
       }
     }
   }
   
   handleClick(e) {
-   
+    this.makeMove(e.target);
   }
 
   makeMove(square) {
+    const row = square.dataset.row;
+    const col = square.dataset.col;
+
+    this.game.playMove([row, col]);
+
+    const currentPlayer = this.game.currentPlayer;
+    this.containerEle.querySelector();
   }
   
   handleGameOver() {
